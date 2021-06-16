5f8fd8f41fe8d1e77dd83c79ddc2e8e20355f03f
Agnieszka Polak <Agnieszka.Polak@cshark.com>
2021-06-16 23:47:26 +0200

    add missing files

---
+++ Todo/Models/Gravatar/GravatarModels.cs
@@ -0,0 +1,29 @@
+﻿namespace Todo.Models.Gravatar
+{
+    public class Response
+    {
+        public Entry[] entry { get; set; }
+    }
+
+    public class Entry
+    {
+        public Name name { get; set; }
+
+        public Photo[] photos { get; set; }
+
+        public string displayName { get; set; }
+    }
+
+    public class Name
+    {
+        public string formatted { get; set; }
+    }
+
+    public class Photo
+    {
+        public string value { get; set; }
+
+        public string type { get; set; }
+    }
+
+}
---
+++ Todo/Services/GravatarApi.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Todo.Models.Gravatar;
+
+namespace Todo.Services
+{
+    public class GravatarApi
+    {
+        private HttpClient httpClient;
+
+        public GravatarApi(HttpClient httpClient)
+        {
+            this.httpClient = httpClient;
+        }
+
+        public async Task<string> GetProfileName(string email)
+        {
+            var response = await httpClient.GetAsync($"{Gravatar.GetHash(email)}.json");
+            if (response.StatusCode == System.Net.HttpStatusCode.NotFound)
+                return "";
+            var content = await response.Content.ReadAsStringAsync();
+            var responseObj = JsonConvert.DeserializeObject<Response>(content);
+            var entry = responseObj.entry.FirstOrDefault();
+            if (entry == null) 
+                return "";
+            return entry?.name.formatted ?? (entry.displayName ?? "");
+        }
+    }
+}
